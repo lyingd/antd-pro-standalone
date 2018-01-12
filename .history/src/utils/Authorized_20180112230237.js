386325794bdb5bb2c3d979c4ce2ea7f85ba5ec0e
ash <lyingd@gmail.com>
2018-01-13 01:04:45 +0800

    ignore .history .vscode

--- .history/src/utils/Authorized_20180112230237.js
+++
@@ -1,5 +0,0 @@
-import RenderAuthorized from 'ant-design-pro/lib/Authorized';
-import { getAuthority } from './authority';
-
-const Authorized = RenderAuthorized(getAuthority());
-export default Authorized;
